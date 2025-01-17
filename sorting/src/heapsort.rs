3a5cfabdb018851bf84446d632480700075da113
Demes Toss <me@demestoss.com>
2025-01-17 10:48:36 +0100

    Heapsort start

--- sorting/src/heapsort.rs
+++ sorting/src/heapsort.rs
@@ -2,11 +2,43 @@ use crate::Sorter;
 
 pub struct HeapSort;
 
+fn heapify<T: Ord>(slice: &mut [T]) {
+    //
+}
+
+fn swap_on_place<T: Ord>(slice: &mut [T], parent_idx: usize) {
+    let left_idx = parent_idx * 2 + 1;
+    let right_idx = parent_idx * 2 + 2;
+
+    if let Some(left) = slice.get(left_idx) {
+        let current = &slice[parent_idx];
+        if left > current {
+            slice.swap(parent_idx, left_idx);
+            swap_on_place(slice, left_idx);
+        }
+    }
+
+    if let Some(right) = slice.get(right_idx) {
+        let current = &slice[parent_idx];
+        if right > current {
+            slice.swap(parent_idx, right_idx);
+            swap_on_place(slice, right_idx);
+        }
+    }
+}
+
 impl Sorter for HeapSort {
     fn sort<T>(&self, slice: &mut [T])
     where
         T: Ord,
     {
+        heapify(slice);
+
+        for i in 1..slice.len() {
+            let last_index = slice.len() - i;
+            slice.swap(0, last_index);
+            swap_on_place(&mut slice[0..last_index], 0);
+        }
     }
 }
 
