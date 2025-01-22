6da3013706431ff22b0d7f5ff09135bad59977ab
Demes Toss <me@demestoss.com>
2025-01-22 23:56:54 +0100

    WIP persistent stack

--- collections/linked-list/src/lib.rs
+++ collections/linked-list/src/lib.rs
@@ -1,4 +1,4 @@
-pub mod first;
-pub mod second;
-pub mod third;
+pub mod first_bad_stack;
+pub mod second_ok_stack;
+pub mod third_persistent_stack;
 
---
+++ collections/linked-list/src/third_persistent_stack.rs
@@ -0,0 +1,20 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+
+}
